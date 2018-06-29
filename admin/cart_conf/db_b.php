50de6438606bf793d45e8739d67735e424bbf710
leovanessa25 <vanessa.leo@thinkopen.it>
2018-06-29 17:26:31 +0200

    edit responsive

--- admin/cart_conf/db_b.php
+++ admin/cart_conf/db_b.php
@@ -1,17 +1,17 @@
 <?php
-	
-	$host = 'localhost';
-	$user = 'root';
-	$password = '';
-	$database = 'wedding_db';
-	
-	
+
+	// $host = 'localhost';
+	// $user = 'root';
+	// $password = '';
+	// $database = 'wedding_db';
+
+
 	// host
-	/*$host = '62.149.150.170';
+	$host = '62.149.150.170';
 	$user = 'Sql922749';
 	$password = 'b108ry364k';
 	$database = "Sql922749_1";
-	*/
+
 	//dati per unbit
 	$con = mysqli_connect($host,$user,$password, $database) or die ("Non riesco a connettermi al DB");
 
